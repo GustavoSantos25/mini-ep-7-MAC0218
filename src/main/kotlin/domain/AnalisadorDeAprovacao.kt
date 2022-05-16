60558125d8ddd05fa9ab0094234afc6609942b28
GustavoSantos25 <gustavo.war2015@gmail.com>
2022-05-15 21:36:00 -0300

    mudanças para passar nos testes

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,5 +1,7 @@
 package domain
 
+import domain.criterios.CriterioDeAprovacao
+
 class AnalisadorDeAprovacao {
 
     // ---------------------------------
@@ -11,5 +13,18 @@ class AnalisadorDeAprovacao {
     //      'src/test/kotlin/domain/AnalisadorDeAprovacaoTest'
     //
     // ---------------------------------
+    private lateinit var criterioAprovacao : CriterioDeAprovacao
+
+    fun defineCriterio(criterio: CriterioDeAprovacao){
+        criterioAprovacao = criterio
+    }
+
+    fun fechaBoletim(boletim: Boletim) : BoletimFechado{
+        val mediaFinal = criterioAprovacao.mediaFinal(boletim)
+        val foiAprovado = criterioAprovacao.estaAprovado(boletim)
+
+        return BoletimFechado(boletim.mediaEPs, boletim.mediaMiniEPs, mediaFinal, foiAprovado)
+    }
+
 
 }
